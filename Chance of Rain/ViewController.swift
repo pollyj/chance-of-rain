9271c7ba9e1555bd00f9ff8d08c7800beb0b2c5c
Yuya Harada <tzou11291996@gmail.com>
2020-05-03 16:14:20 +0900

    Initial Commit

---
+++ Chance of Rain/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Chance of Rain
+//
+//  Created by Polly Sutcliffe & Yuya Harada on 2020/05/03.
+//  Copyright © 2020 Polly Sutcliffe & Yuya Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    // declare outlet
+
+    @IBOutlet weak var location: UILabel!
+    @IBOutlet weak var mainWeatherIcon: UIImageView!
+    @IBOutlet weak var mainTemperature: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
